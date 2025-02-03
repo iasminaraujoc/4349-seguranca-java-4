9be4a1a621c7b4578189cb433b688a8c7073d020
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2025-02-03 17:05:27 -0300

    video 2.1

--- src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGithubService.java
+++ src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGithubService.java
@@ -1,5 +1,6 @@
 package br.com.forum_hub.domain.autenticacao.github;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -25,7 +26,7 @@ public class LoginGithubService {
                 "&scope=read:user,user:email";
     }
 
-    public String obterToken(String code) {
+    private String obterToken(String code) {
         var resposta = restClient.post()
                 .uri("https://github.com/login/oauth/access_token")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -33,7 +34,23 @@ public class LoginGithubService {
                 .body(Map.of("code", code, "client_id", clientId,
                         "client_secret", clientSecret, "redirect_uri", redirectUri))
                 .retrieve()
+                .body(Map.class);
+        return resposta.get("access_token").toString();
+    }
+
+    public String obterEmail(String code){
+        var token = obterToken(code);
+
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        var resposta = restClient.get()
+                .uri("https://api.github.com/user/emails")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
                 .body(String.class);
+
         return resposta;
     }
 }
