75d94d5360c5d5cb593601f6b19978acb7ae2ff9
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2025-02-05 17:10:34 -0300

    video 5.3

--- src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -1,14 +1,17 @@
 package br.com.forum_hub.controller;
 
+import br.com.forum_hub.domain.autenticacao.DadosA2F;
 import br.com.forum_hub.domain.autenticacao.DadosLogin;
 import br.com.forum_hub.domain.autenticacao.DadosRefreshToken;
 import br.com.forum_hub.domain.autenticacao.DadosToken;
 import br.com.forum_hub.domain.autenticacao.TokenService;
 import br.com.forum_hub.domain.usuario.Usuario;
 import br.com.forum_hub.domain.usuario.UsuarioRepository;
+import br.com.forum_hub.infra.seguranca.totp.TotpService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +24,13 @@ public class AutenticacaoController {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
     private final UsuarioRepository usuarioRepository;
+    private final TotpService totpService;
 
-    public AutenticacaoController(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioRepository usuarioRepository) {
+    public AutenticacaoController(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioRepository usuarioRepository, TotpService totpService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
         this.usuarioRepository = usuarioRepository;
+        this.totpService = totpService;
     }
 
     @PostMapping("/login")
@@ -38,6 +43,20 @@ public class AutenticacaoController {
             return ResponseEntity.ok(new DadosToken(null, null, true));
         }
 
+        String tokenAcesso = tokenService.gerarToken(usuario);
+        String refreshToken = tokenService.gerarRefreshToken(usuario);
+
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken, false));
+    }
+
+    @PostMapping("/verificar-a2f")
+    public ResponseEntity<DadosToken> verificarSegundoFator(@Valid @RequestBody DadosA2F dadosA2F){
+        var usuario = usuarioRepository.findByEmailIgnoreCaseAndVerificadoTrueAndAtivoTrue(dadosA2F.email())
+                .orElseThrow();
+        var codigoValido = totpService.verificarCodigo(dadosA2F.codigo(), usuario);
+        if(!codigoValido){
+            throw new BadCredentialsException("Código inválido");
+        }
         String tokenAcesso = tokenService.gerarToken(usuario);
         String refreshToken = tokenService.gerarRefreshToken(usuario);
 
